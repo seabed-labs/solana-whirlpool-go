c05de3827993be60b95859a30ec0b94306d7cc4d
Mocha Dcaf <mocha@dcaf.so>
2023-01-29 18:52:44 -0500

    fix: add default programId

--- pkg/v0/instructions.go
+++ pkg/v0/instructions.go
@@ -12,7 +12,7 @@ import (
 	ag_treeout "github.com/gagliardetto/treeout"
 )
 
-var ProgramID ag_solanago.PublicKey
+var ProgramID ag_solanago.PublicKey = ag_solanago.MustPublicKeyFromBase58("whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc")
 
 func SetProgramID(pubkey ag_solanago.PublicKey) {
 	ProgramID = pubkey
